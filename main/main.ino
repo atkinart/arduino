fa0585c67f22cbd07d68539555fc645c829858c1
aatkin <a.atkin@isource.ru>
2025-07-04 22:21:05 +0300

    align screen text

--- main/main.ino
+++ main/main.ino
@@ -5,7 +5,7 @@ float H = 0;
 #include "ASOLED.h"
 #include "DHT.h"
 
-DHT dht_4(4, DHT11);
+DHT dht_4(4, DHT22);
 
 void setup() {
   LD.init();
@@ -19,14 +19,16 @@ void loop() {
     H = dht_4.readHumidity();
     
     // Вывод температуры
-    LD.printString_12x16("", 0, 0);
+    LD.printString_12x16("Темп ", 0, 0);
+    LD.printString_12x16("", 70, 0);
     LD.printNumber((long)T);
-    LD.printString_12x16("C°", 40, 0);
+    LD.printString_12x16("C°", 100, 0);
     
     // Вывод влажности
-    LD.printString_12x16("", 80, 5);
-    LD.printNumber((long)H);
     LD.printString_12x16("ВЛАГА", 0, 5);
+    LD.printString_12x16("", 70, 5);
+    LD.printNumber((long)H);
+    LD.printString_12x16("%", 100, 5);
     
     // Очистка области сообщения (16 пробелов)
     LD.printString_6x8("                ", 0, 11);
